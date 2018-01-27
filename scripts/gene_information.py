2496ae3c34e6b222a56bbb9d0aafa584d5c5da0d
msplinter46 <marcia_210194@hotmail.com>
2018-01-27 22:36:00 +0000

    atualizados

---
+++ scripts/gene_information.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 10:32:29 2018
+
+@author: asus
+"""
+
+from Bio import SeqIO
+
+
+def ids():              #função que retorna a lista de ID's dos genes de interesse
+    genesid=['19834097','19831637','19833099','19834533', '19833742', '19833208', '19833867', '19833377']
+    return genesid
+
+genesid=ids()
+
+
+def parse_ficheiro(filename):               #parse do ficheiro obtido na página do genoma completo, accession = NC_002942.5
+    records=SeqIO.parse(open(filename), "genbank")
+    return records
+    
+def gerar_info(records):            #gera uma tabela com informações do gene e proteina e grava a tabela num ficheiro.
+                                    #Grava também a sequencia de aminoacidos num outro ficheiro
+    res=[]
+    res.append("GeneID"+"\t"+ "Accession Number"+"\t"+"Locus Tag"+"\t"+"Gene"+"\t"+"Strand"+"\t"+
+               "Protein accession (NCBI)"+"\t"+"Protein ID (UniProt)"+"\t"+"Entry Name"+"\t"+"Revision"+"\t"+"Protein Name"+
+               "\t"+"Protein length"+"\t"+"Celular location"+ "\t"+"EC Number/TC Number"+"\t"+"Comments" +"\t"+
+                "Features" +"\t"+ "Keywords" +"\t"+"Cross References"+"\n" )
+    seqs=open("sequencias.fasta","w")
+    for r in records:
+        for a in r.features:
+            if a.type == "CDS" and a.qualifiers["db_xref"][0][7::] in genesid:
+                gene_id=str(a.qualifiers["db_xref"][0][7::])
+                accession="NC_002942.5"
+                locus=str(a.qualifiers['locus_tag'][0])
+                if "gene" in a.qualifiers:              
+                    gene_name=str(a.qualifiers['gene'][0])
+                else:
+                    gene_name= "null"            
+                strand=str(a.strand)
+                protein_id=str(a.qualifiers['protein_id'][0])
+                res.append(gene_id+"\t"+ accession+"\t"+ locus +"\t"+ gene_name +"\t"+ strand +"\t"+ protein_id +"\t"+ "\n") 
+                seqs.write(">"+str(protein_id+"\n"))
+                seqs.write(str(a.qualifiers['translation'][0]))
+                seqs.write("\n")   
+    f= open("information.txt", "w")
+    for lin in res:
+        f.write(lin)  
+    f.close()
+    seqs.close()
+
+
+def teste():
+    f=parse_ficheiro("sequence.gb")
+    gerar_info(f)
+
+teste()
+
+
--- scripts/protein_treatment.py
+++ scripts/protein_treatment.py
@@ -5,12 +5,11 @@ Created on Wed Jan 24 10:54:51 2018
 @author: asus
 """
 from re import search
-from re import finditer
 from Bio import ExPASy
 from Bio import SwissProt
 
 
-def ncbi_to_uniprot():
+def ncbi_to_uniprot():       #retorna um dicionario em que as chaves são os id's dos genes e os valores são os id's das proteinas na uniprot
     w = open("ncbiTOuniprot.txt","r")
     d = {}
     for l in w.readlines():
@@ -19,75 +18,47 @@ def ncbi_to_uniprot():
     w.close()
     return d
 
-
-def swissprot(geneid, d):
-    res=[]
+def gerar_protein_info(ids):        #procura a proteina na uniprot e gera várias informações que são depois adicionadas a uma lista
     try:
-        handle = ExPASy.get_sprot_raw(geneid)
+        handle = ExPASy.get_sprot_raw(ids)
         swiss_record = SwissProt.read(handle) #Cria objeto SwissProt.Record
-        entry_name="Entry name: " + str(swiss_record.entry_name)+"\n"
-        length="Protein length: "+ str(swiss_record.sequence_length)+"\n"
-        description="Description: "+ str(swiss_record.description).strip("RecName: Full=")+"\n"
-        features="Features: "+  str(swiss_record.features)+"\n"
-        keywords="Keywords: "+ str(swiss_record.keywords)+"\n"
-        annotations= "Annotations: " + str(swiss_record.annotation_update)+"\n"
-        res.append(entry_name + length + description + features + keywords + annotations)      
+        entry_name=str(swiss_record.entry_name)
+        length=str(swiss_record.sequence_length)
+        description=str(swiss_record.description).strip("RecName: Full=")
+        features=str(swiss_record.features).strip("']")
+        features=features.strip("['")
+        keywords=str(swiss_record.keywords).strip("']")
+        keywords=keywords.strip("['")
+        accessions=str(swiss_record.accessions).strip("']")
+        accessions=accessions.strip("['")
+        comments=str(swiss_record.comments).strip("']")
+        comments=comments.strip("['")
+        cross_refs=str(swiss_record.cross_references).strip("']")
+        cross_refs=cross_refs.strip("['")
+        dataclass=str(swiss_record.data_class)      
         handle.close()
-        return ''.join(str(e) for e in res)
     except Exception as e:
        pass
-
-def gerar_protein_info(filename):
-    fh = open(filename+".txt", "r")
-    regex_id= "AC \s* (.*);"
-    regex_revision= "ID \s (.*) \s (.*);"
-    regex_pname= "Full=(.*)\s{"
     regex_location= "SUBCELLULAR LOCATION:\s(.*)\s{.*\."
     regex_ec= "EC=(.*){.*;"
-    regex_function= "FUNCTION:\s*(.*)((\s(.*))*)"
     l=[]
-    lines=fh.readlines()
-    string=''.join(str(e) for e in lines)
-
-    if search(regex_id, string):
-        ids=search(regex_id, string).group(1)
-    if search(regex_revision, string):
-        revision= search(regex_revision, string).group(2)
-    if search(regex_pname, string):
-        pname=search(regex_pname, string).group(1)
-    else:
-        regex_pname="Full=(.*);"
-        pname=pname=search(regex_pname, string).group(1)
-    if search(regex_location, string):
-        location=search(regex_location, string).group(1)
+    if search(regex_location, comments):            #encontra a localização da proteina, e depois o ec number de forma automatica
+        location=search(regex_location, comments).group(1)
     else:
         location="Unknown"
-    if search(regex_ec, string):
-        ec=search(regex_ec, string).group(1)
+    if search(regex_ec, description):
+        ec=search(regex_ec, description).group(1)
     else:
-        ec="Not available"
-    if search(regex_function, string):
-        function= search(regex_function, string).group()
-        func_aux=finditer(r"ECO:.*\s.*CATALYTIC ACTIVITY:", function)
-        for x in func_aux:
-            fim=x.span()[0]
-            function=function[10:fim]
-        function=function.replace("\n","")
-        function=function.replace("CC","")
-        function=function.replace("{","")
-        function=function.replace("   ", "")
-        function=function.rstrip()        
-    else:
-        function="Unknown"
-    l.append(ids+"|"+ "|"+revision+"|"+pname+"|"+ "|"+location+"|"+ec+"|"+function+"|")
+        ec="Unfound"
+    l.append(accessions+"\t"+entry_name+"\t"+dataclass+"\t"+description+"\t"+ length+"\t"+location+"\t"
+             +ec+"\t"+comments +"\t"+ features +"\t"+ keywords+"\t"+cross_refs+"\t")
     protein_info=''.join(str(e) for e in l)
     return protein_info
     
     
-def add_to_table():
+def add_to_table():         #adiciona as informações obtidas na função anterior à tabela relativa aos genes
     fh= open("information.txt", "r")
     final=open("final_table.txt", "w")
-    file=open("swissprot.txt","w")
     lines=[]
     final.write(fh.readline())
     for line in fh:
@@ -99,11 +70,9 @@ def add_to_table():
             if search(key, line):
                 line+=gerar_protein_info(d[key])
                 final.write(line+"\n")
-                file.write(swissprot(d[key],d)+"\n")  
+                  
     fh.close()
     final.close()
-
-
 
 add_to_table()
     
